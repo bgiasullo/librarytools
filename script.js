9146902776a2d04b9ec5ac67b81fb7087c63dca2
Brig <76627609+bgiasullo@users.noreply.github.com>
2025-11-29 09:01:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ function processCSV() {
       const url = URL.createObjectURL(blob);
       const a = document.createElement("a");
       a.href = url;
-      a.download = "final_text.csv";
+      a.download = "cleaned-zooniverse-data.csv";
       a.click();
       URL.revokeObjectURL(url);
 
